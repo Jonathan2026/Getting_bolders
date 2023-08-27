58720cb9e59ed34361291544bf974542e82a0497
Jonathan2026 <139922118+Jonathan2026@users.noreply.github.com>
2023-08-26 17:59:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -86,14 +86,11 @@ const player = new Player()
 
 const bubblesArray = []
 
-const bolder = new Image()
-bolder.src = "blue_small.png"
-
 class Bubble {
     constructor(){
         this.x = Math.random() * canvas.width
         this.y = canvas.height + Math.random() * canvas.height
-        this.radius = 70
+        this.radius = 50
         this.speed = Math.random() * 5 + 1
         this.distance;
         this.count = false
@@ -109,7 +106,7 @@ class Bubble {
     draw(){
         ctx.fillStyle = 'blue'
         ctx.beginPath()
-        ctx.drawImage(bolder, this.x, this.y)
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
         ctx.fill()
         ctx.closePath()
         ctx.stroke()
@@ -144,12 +141,70 @@ function handleBubbles(){
 //__________________________________________________________-End of Bubbles______________________________________________________________
 
 
+//________________________________________________________Enemies___________________________________________________________________________
+
+const enemyArray = [] 
+
+class Enemies{
+    constructor(){
+        this.x = 0
+        this.y = Math.random() * canvas.height
+        this.radius = 50
+        this.speed = Math.random() * 5 + 1
+        this.distance;
+    }
+    
+    update(){
+        this.x += this.speed
+        const dxEnemies = this.x - player.x
+        const dyEnemies = this.y - player.y
+        this.distance = Math.sqrt(dxEnemies * dxEnemies + dyEnemies * dyEnemies)
+    }
+
+    draw(){
+        ctx.fillStyle = 'black'
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
+        ctx.fill()
+        ctx.closePath()
+        ctx.stroke()
+    }
+}
+
+
+function handleEnemies(){
+    if (gameFrame % 300 == 0){
+        console.log("enemy")
+        enemyArray.push(new Enemies())
+    }
+
+    for (let i = 0; i < enemyArray.length; i++){
+        enemyArray[i].update()
+        enemyArray[i].draw()
+        
+        if(enemyArray[i].x > 800){
+            enemyArray.splice(i, 1)
+
+        }
+        else if (enemyArray[i].distance < enemyArray[i].radius + player.radius){
+            score = 0
+            enemyArray.splice(i, 1)
+            
+        }
+    }
+}
+
+//___________________________________________________________End of Enemies_____________________________________________________________________________________
+
+
 //__________________________________________________Animation loop______________________________________________________
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    handleEnemies();
     handleBubbles();
     player.update();
     player.draw();
+
 
     ctx.fillText("Score: " + score, 10, 50)
 
